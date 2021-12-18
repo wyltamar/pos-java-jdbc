a176a01a113b35b804f8e3421735d57b49c8c932
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2021-12-18 11:36:18 -0300

    Recuperando classe teste

--- pos-java-jdbc/src/main/java/pos_java_jdbc/pos_java_jdbc/TesteBancoJdbc.java
+++ pos-java-jdbc/src/main/java/pos_java_jdbc/pos_java_jdbc/TesteBancoJdbc.java
@@ -1,12 +1,3 @@
 package pos_java_jdbc.pos_java_jdbc;
 
-import conexaojdbc.SingleConnection;
 
-public class TesteBancoJdbc {
-	
-	
-	public void initBanco() {
-		SingleConnection.getConnection();
-	}
-
-}
